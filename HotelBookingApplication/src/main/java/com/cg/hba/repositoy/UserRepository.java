34905b9b9b4dadfb2d27537b5a7d216a1c16e8a5
Akilan24 <akilan24012001@gmail.com>
2022-09-02 11:39:25 +0530

    remove user

--- HotelBookingApplication/src/main/java/com/cg/hba/repositoy/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cg.hba.repositoy;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.cg.hba.entity.User;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-
-}
